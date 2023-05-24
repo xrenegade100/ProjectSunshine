7d97632a5ec83dd1ed01bbf92470ea4fc5f74b8a
Antonio Scognamiglio <antonio.scognamiglio12@outlook.it>
2023-05-24 15:54:09 +0200

    update submodules configuration

--- packages/vscode-estension/README.md
+++
@@ -1,2 +0,0 @@
-# greet
-# greet-vscode
--- packages/vscode-estension/src/extension.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-unresolved
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {}
-
-export function deactivate() {}
